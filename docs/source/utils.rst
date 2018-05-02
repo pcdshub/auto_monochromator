ac10e5bfba1aaf57635655c351e2d8d3851fe434
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2018-05-02 16:23:27 -0700

    Initial commit. Freshly baked from pcdshub/cookiecutter-pcds-python.

---
+++ docs/source/utils.rst
@@ -0,0 +1,8 @@
+Utility Functions
+=================
+
+.. autoclass:: auto_monochromator.utils.RotatingFileHandlerRelativePath
+
+.. autofunction:: auto_monochromator.utils.absolute_submodule_path
+
+.. autofunction:: auto_monochromator.utils.setup_logging
---
+++ setup.py
@@ -0,0 +1,11 @@
+import versioneer
+from setuptools import setup, find_packages
+
+setup(name='auto_monochromator',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      license='BSD',
+      author='SLAC National Accelerator Laboratories',
+      packages=find_packages(),
+      description='Toolset for automated mochromator tuning',
+      )
