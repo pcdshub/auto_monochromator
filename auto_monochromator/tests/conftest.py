24f3e6fa8ae3705aa8f55ece5e36b75774cad8aa
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2018-05-11 17:38:07 -0700

    DEV/TST: RapidHist implemented RapidTransmissionHist coming soon.

--- auto_monochromator/tests/conftest.py
+++ auto_monochromator/tests/conftest.py
@@ -6,11 +6,10 @@ import h5py
 import os
 logger = logging.getLogger(__name__)
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def sxrR6_dataFrame():
  
     directory = os.path.dirname(os.path.realpath(__file__))
     test_file_path = os.path.join(directory, "sxrx30116run6_slim.h5") 
-    
     file = h5py.File(test_file_path)
     return file
