7e6ac9cf79225e819e16633e663504a9924fa975
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2018-05-23 15:37:24 -0700

    DEV:plotting work moved to tornado loop

--- auto_monochromator/bokeh_monitor.py
+++ auto_monochromator/bokeh_monitor.py
@@ -12,12 +12,15 @@ from pcdsdevices import beam_stats
 from auto_monochromator.rapid_stats import RapidHist
 import numpy as np
 from tornado.ioloop import PeriodicCallback
+import time
 
-def modify_doc(doc,incident):
+def modify_doc(doc,plot_data):
     
     #d = figure(x_range=(0, 100), y_range=(0, 100))
     #d = figure()
-    d = figure(x_range=(510, 550),y_range=(-50,500))
+    
+    #d = figure(x_range=(510, 550),y_range=(-50,500))
+    d = figure(y_range=(-50,500))
     k = d.quad(
         top=[],
         bottom=[],
@@ -32,41 +35,46 @@ def modify_doc(doc,incident):
     
     m = s()
     m.i = 0
-
+    '''
     hist = RapidHist(
         # 1000 just seems to be the magic number for the accel_ev 
         maxlen=1000,
-        bins = np.arange(510,550,.5)
+        bins = np.arange(6850,6950,.5)
     )
-        
+    '''
 
-    def callback(k_obj,ds,hist):
+    def callback(k_obj, hist_data):
         k_obj.i += 1
-        hist.push(ds)
-        print(k_obj.i,"\t",len(hist._data),"\t",len(ds))
-        ds.clear()
+        print(k_obj.i)
         nd = dict()
-        hs, bins = hist.hist()
-        # nd['top'] = [random()*100]
-        # nd['bottom'] = [random()*100]
-        # nd['left'] = [random()*100]
-        # nd['right'] = [random()*100]
-        nd['top'] = hs
-        nd['bottom'] = np.zeros(len(hs))
-        nd['left'] = bins[:-1]
-        nd['right'] = bins[1:]
+        nd['top'] = hist_data.hs
+        nd['bottom'] = np.zeros(len(hist_data.hs))
+        nd['left'] = hist_data.bins[:-1]
+        nd['right'] = hist_data.bins[1:]
         ks.data = nd
     
     
     # add a button widget and configure with the call back
     # button = Button(label="Press Me")
     # button.on_click(callback)
-    doc.add_periodic_callback(partial(callback,k_obj=m,ds=incident,hist=hist),100)
+    doc.add_periodic_callback(
+        partial(callback,k_obj = m,hist_data=plot_data),
+        100
+    )
     # put the button and plot in a layout and add to the document
     doc.add_root(column(d))
 
-def random_print():
-    print(np.random.random(5))
+def pull_data(ds, hist, out):
+    print('recalc plot\t', len(ds),'\t', time.ctime(),end='\t')
+    hist.push(ds)
+    print(len(hist._data))
+    ds.clear()
+    out.hs, out.bins = hist.hist()
+
+class Carrier:
+    def __init__(self):
+        self.hs = None
+        self.bins = None
 
 # Setting num_procs here means we can't touch the IOLoop before now, we must
 # let Server handle that. If you need to explicitly handle IOLoops then you
@@ -82,10 +90,23 @@ def launch_server():
     stats.accel_ev.subscribe(
         partial(append_to_data_block,inc_data_block=inc_data_block)
     )
-   
+    '''
+    stats.mj.subscribe(
+        partial(append_to_data_block,inc_data_block=inc_data_block)
+    )
+    '''
+
+
+    hist = RapidHist(
+        maxlen=1000,
+        bins = np.arange(975,1025,.5),
+        #bins = np.arange(-.1,.1,.005),
+    )
+    
+    m = Carrier()
 
     server = Server(
-        {'/': partial(modify_doc,incident=inc_data_block)},
+        {'/': partial(modify_doc,plot_data=m)},
         num_procs=1
    )
     server.start()
@@ -93,7 +114,10 @@ def launch_server():
 
     print('Opening Bokeh application on http://localhost:5006/')
     #server.io_loop.PeriodicCallback(random_print,500).start()
-    pcall = PeriodicCallback(random_print,100)
+    pcall = PeriodicCallback(
+        partial(pull_data, ds=inc_data_block, hist=hist, out=m),
+        100
+    )
     pcall.start()
     print(type(server.io_loop))
     #server.io_loop.add_callback(server.show, "/")
