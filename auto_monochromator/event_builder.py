85d51e9f49b9ba0e3fd18190be529c797350ac85
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2018-05-24 14:31:49 -0700

    DEV/TST: Add mini-eventbuilder

--- auto_monochromator/event_builder.py
+++ auto_monochromator/event_builder.py
@@ -1,8 +1,15 @@
 import numpy as np
+import pandas as pd
 from collections import deque
 
-
-
-        
-
+def basic_event_builder(*args,**kwargs):
+    """
+    Pass any number of data frames and return an event built pandas DataFrame.
+    Kwargs can be used to name the columns of the returned DataFrame.
+    """
+    data_table = dict()
+    [data_table.setdefault(col,args[col]) for col in range(len(args))]
+    [data_table.setdefault(col,kwargs[col]) for col in kwargs]
+    full_frame = pd.DataFrame(data_table)
+    return full_frame.dropna()
 
--- auto_monochromator/tests/test_event_builder.py
+++ auto_monochromator/tests/test_event_builder.py
@@ -3,6 +3,36 @@ import pandas as pd
 import numpy as np
 from auto_monochromator.rapid_stats import (RapidHist, RapidWeightHist,
     RapidTransmissionHist)
+from auto_monochromator.event_builder import basic_event_builder
 from collections import deque
 logger = logging.getLogger(__name__)
+
+
+
+def test_simple_event_builder():
+    # missing 1,8
+    at = [1,2,3]
+    ad = [1,2,3]
+    # missing 1,3,7,9
+    bt = [1,2,4]
+    bd = [1,2,4]
+    series_a = pd.Series(ad,index=at)
+    series_b = pd.Series(bd,index=bt)
+    result = basic_event_builder(series_a, second=series_b)
+    target = pd.DataFrame(
+        {
+            0: [1,2],
+            'second': [1,2]
+        },
+        index = [1,2]
+    )
+    print(target)
+    a_result = result[0] == target[0]
+    b_result = result['second'] == target['second']
+    index_result = result.index == target.index
+    col_result = result.columns == target.columns
+    assert a_result.all()
+    assert b_result.all()
+    assert index_result.all()
+    assert col_result.all()
 
